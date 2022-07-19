b0b54e41840e722a45d0c1d52b3033024009c19a
Jonathan Hop <yithril@gmail.com>
2022-07-18 21:04:19 -0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 import { ListPetsComponent } from './components/list-pets/list-pets.component';
 import { PetDetailsComponent } from './components/pet-details/pet-details.component';
 
 const routes: Routes = [
   { path: "listPets", component: ListPetsComponent },
-  { path: "petDetails/:id", component: PetDetailsComponent},
+  { path: "petDetails/:id", component: PetDetailsComponent, canActivate: [AuthGuard]},
   { path:'', redirectTo:'listPets', pathMatch: 'full' },
 ];
 
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Pet } from '../interfaces/pet';
